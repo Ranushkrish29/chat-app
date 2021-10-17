f91d63f5cf837847f6fd1cdce81eb44a58ffc848
Ranush Krishna <ranush.krishna@gmail.com>
2021-10-17 12:48:43 +0530

    started

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          hi ,there this is my first chat app using firebase with reactjs
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
